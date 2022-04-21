b6827e65318d54b8065cb7497f69ad8b7ee02b2a
muldrow1975 <ntmuldrow@yahoo.com>
2022-04-21 12:25:28 -0400

    used props for multiple props first time

--- src/components/portfolio-items/portfolio-container.js
+++ src/components/portfolio-items/portfolio-container.js
@@ -12,17 +12,37 @@ export default class PortfolioContainer extends Component {
         const data = ["Shell", "Bp", "Exxon"];
 
         return data.map(item => {
-            return <h2>{item}</h2>
+            return <PortfolioItem thanos={item}  url={"google.com"} />
         });
     }
+
+    portfolioItems2() {
+        const data = ["United States ", "British", "Swedish"];
+
+        return data.map(item => {
+            return <PortfolioItem mark={item} />
+        });
+    }
+
+    portfolioItems3() {
+        const data = ["Alabama classs ", "Thames class", "Norse Class"];
+
+        return data.map(item => {
+            return <PortfolioItem jarvis={item} />
+        });
+    }
+
     //State
     //Lifecycle hooks
     render() {
         return(
             <div>
                 <h2> Portfolio goes here...</h2>
+                
 
                 {this.portfolioItems()}
+                {this.portfolioItems2()}
+                {this.portfolioItems3()}
             </div>
         )
 
--- src/components/portfolio-items/portfolio-item.js
+++ src/components/portfolio-items/portfolio-item.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function() {
+export default function(props) {
     return (
         <div>
             <h3>Portfolio Item</h3>
+            <h4>{props.thanos}</h4>
+            <h5>{props.url}</h5>
+            <h6>{props.mark}</h6>
+            <h7>{props.jarvis}</h7>
         </div>
     );
 

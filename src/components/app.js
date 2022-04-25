24df78dc33ad261e0480cf27770d88a21d40dc0e
muldrow1975 <ntmuldrow@yahoo.com>
2022-04-25 18:39:37 -0400

    new plop

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
 
-import PortfolioContainer from './portfolio-items/portfolio-container';
+import PortfolioContainer from "./portfolio-items/portfolio-container";
+import NavigationContainer from "./navigation-container/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Sonic React Portfolio</h1>
+      <div className="app">
+        <NavigationContainer />
+        <h1>Iron Studios Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <h2> Why is so sad!</h2>
         <PortfolioContainer />
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-
-        </div>
-        <h2>React Redux Router</h2>
       </div>
     );
   }
 }
+
+

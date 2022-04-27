895bb4b37168cb192816ed6e4d10ff5bab88fabf
muldrow1975 <ntmuldrow@yahoo.com>
2022-04-27 16:42:32 -0400

    images placed

--- src/components/portfolio-items/portfolio-item.js
+++ src/components/portfolio-items/portfolio-item.js
@@ -8,11 +8,11 @@ import { Link } from "react-router-dom";
   // - id: id
 
 export default function(props) {
-    const { id, description, thumb_image_url, logo } = props.item;
+    const { id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div>
       <img src={thumb_image_url} />
-      <img src={logo} />
+      <img src={logo_url} />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link</Link>
     </div>
